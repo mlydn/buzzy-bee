7ff1fdfcee4922d0d5ab2335ee898e3423b11bf8
mlydn <59616364+mlydn@users.noreply.github.com>
2024-11-15 17:44:23 +0100

    Collapsability

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import header from './assets/header.jpeg'
 import './App.css'
+import {useCollapse} from 'react-collapsed';
 
 function App() {
-
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
   return (
     <>
       <div id="header">
@@ -39,6 +40,18 @@ function App() {
           </ul>
         </p>
       </div>
+      <div className="collapsible card">
+        <div className="header" {...getToggleProps()}>
+            <h2>About {isExpanded ? <img height="20" width="20" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAz0lEQVR4nO3YywrCMBBG4fOE1oWDC/vsCiq+RAuVQLpzvDVtJvAfCJSki/nIqgWllFJKKaUqtwfuwDU/N5kBAzDlNQI9jSOmFjHmIJrCHF8gBmcvvdvMTYzACTg4Z31LiLnwGPsCER5jPyDCYuwPRDiMLUCEwVgBRHWMFURUw9gKiM0xtiJiM4xtgFgdYxsi5opjaiCKY2oiimEiIBZjuoDfDkdnpjSr2y0Y4h0m/dBwewREeJg0q1uXpedPV1epDrjkGXe1h1FKKaWUUrTVE5bz7NyN3sYLAAAAAElFTkSuQmCC" alt="collapse" /> : <img height="20" width="20" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA3klEQVR4nO3YSwrCMBRG4eMOVQwZ6OoVQfGxgQqVQgsiSe0zucX/QGdJuR+hgxSUUkoppZTqmQPuwBlYY68NcAGegI8tWgE3oKyfom1xhnw9UzPfo5452PVjoSWM/0KU9azRtoENuTE+MlM1a2u7wMYXcCDPN1sEZtl3fYEFjBuLsIBxUyFyYtzUiBwYNxciJWZ2RApMMsScmOSIOTDZEFNisiOmwJhBjMGYQwzBmEX0wZhHdMEsBvHr7mDtjjP4ZBZzEl0xi0LEMItENFW/lE7A0ejvJaWUUkop9U+9AdnJ7S7GX9jsAAAAAElFTkSuQmCC" alt="expand"></img>}</h2>
+        </div>
+        <div {...getCollapseProps()}>
+            <div className="content">
+              <p>I am an experienced part-qualified accountant (CAT - Certified Accounts Technician) with e demonstrated history of working for small businesses and within Investment Banking.  My strengths include helping to start up any financial system set-ups, improvements or automation of new or current processes, business administration, financial forecasting, financial control, management accounting, and performance analysis.</p>
+              <p>I offer a full remote bookkeeping and business administration service using Moneybird.  I have a range of different pricing and service options to suit all sizes of business.  If you need a couple of hours or up to a couple of days - I can help!</p>
+              <p>Please get in touch with your enquiry.</p>
+            </div>
+        </div>
+    </div>
       <div>
         Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
       </div>
