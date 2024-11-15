a959eb100af0a3f2ddf88b0879d17bcee8b4ce40
mlydn <59616364+mlydn@users.noreply.github.com>
2024-11-15 16:12:34 +0100

    Superstylin

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,24 @@ function App() {
         <p>
           <ul>
             <li id="book">
-              <strong>Comprehensive Bookkeeping:</strong> From recording daily transactions to managing ledgers, we ensure your financial records are always up-to-date and accurate
+              <strong>Comprehensive Bookkeeping:</strong>
+              <span className="description">From recording daily transactions to managing ledgers, we ensure your financial records are always up-to-date and accurate</span>
             </li>
             <li id="health-graph">
-              <strong>Financial Reporting:</strong> We provide detailed financial statements that give you a clear view of your business&apos;s financial health
+              <strong>Financial Reporting:</strong>
+              <span className="description">We provide detailed financial statements that give you a clear view of your business&apos;s financial health</span>
             </li>
             <li id="ledger">
-              <strong>Accounts Payable & Receivable:</strong> Efficient management of your payable and receivables to optimize cash flow
+              <strong>Accounts Payable & Receivable:</strong>
+              <span className="description">Efficient management of your payable and receivables to optimize cash flow</span>
               </li>
             <li id="bank-euro">
-              <strong>Bank Reconciliation:</strong> Regular reconciliation of your bank statements to prevent discrepancies and ensure accuracy
+              <strong>Bank Reconciliation:</strong>
+              <span className="description">Regular reconciliation of your bank statements to prevent discrepancies and ensure accuracy</span>
             </li>
             <li id="accounting">
-              <strong>Tax Preparation & Filing:</strong> Timely and precise tax preparation to keep you compliant and avoid penalties
+              <strong>Tax Preparation & Filing:</strong>
+              <span className="description">Timely and precise tax preparation to keep you compliant and avoid penalties</span>
               </li>
           </ul>
         </p>
