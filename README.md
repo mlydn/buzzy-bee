b1f3a17257615ae0b1df4367cb884d3021a6bd3d
mlydn <59616364+mlydn@users.noreply.github.com>
2024-11-15 14:32:03 +0100

    Add icons and additional styling

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # buzzy-bee
+
+https://vite.dev/
+https://react.dev/
+https://aws.amazon.com/getting-started/hands-on/host-static-website/
+
+AWS Amplify will build your source code and deploy your app at https://...amplifyapp.com, and on every git push your deployment instance will update. It may take up to 5 minutes to deploy your app.
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div id="header">
         <img src={header} />
       </div>
       <h1>Buzzy Bee Bookkeeping</h1>
@@ -16,16 +16,26 @@ function App() {
         <h2>Our Services Include:</h2>
         <p>
           <ul>
-            <li><strong>Comprehensive Bookkeeping:</strong> From recording daily transactions to managing ledgers, we ensure your financial records are always up-to-date and accurate</li>
-            <li><strong>Financial Reporting:</strong> We provide detailed financial statements that give you a clear view of your business's financial health</li>
-            <li><strong>Accounts Payable & Receivable:</strong> Efficient management of your payable and receivables to optimize cash flow</li>
-            <li><strong>Bank Reconciliation:</strong> Regular reconciliation of your bank statements to prevent discrepancies and ensure accuracy</li>
-            <li><strong>Tax Preparation & Filing:</strong> Timely and precise tax preparation to keep you compliant and avoid penalties</li>
+            <li id="book">
+              <strong>Comprehensive Bookkeeping:</strong> From recording daily transactions to managing ledgers, we ensure your financial records are always up-to-date and accurate
+            </li>
+            <li id="health-graph">
+              <strong>Financial Reporting:</strong> We provide detailed financial statements that give you a clear view of your business&apos;s financial health
+            </li>
+            <li id="ledger">
+              <strong>Accounts Payable & Receivable:</strong> Efficient management of your payable and receivables to optimize cash flow
+              </li>
+            <li id="bank-euro">
+              <strong>Bank Reconciliation:</strong> Regular reconciliation of your bank statements to prevent discrepancies and ensure accuracy
+            </li>
+            <li id="accounting">
+              <strong>Tax Preparation & Filing:</strong> Timely and precise tax preparation to keep you compliant and avoid penalties
+              </li>
           </ul>
         </p>
       </div>
       <div>
-      <a target="_blank" href="https://icons8.com/icon/1UXA0mGrf9CT/bee">Bee</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+        Icons by <a target="_blank" href="https://icons8.com">Icons8</a>
       </div>
     </>
   )
